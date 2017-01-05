70f7571fafa0f4a8029aca79617a9aa924026b27
薛定谔的猫 <zhaoshengdong@meituan.com>
2017-01-05 17:11:41 +0800

    update

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -23,12 +23,12 @@
 <script>
 export default {
     name: 'hello',
-    data {
-    return {
-        msg: 'Welcome to Your Vue.js App'
-    };
+    data: function () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        };
     }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
